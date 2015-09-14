49629ed4b0d4876296637932fc416eb8218b3a51
Ed Summers <ehs@pobox.com>
2015-09-13 21:17:25 -0400

    fixed footer + pagination

--- sync.sh
+++ sync.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-jekyll build --full-rebuild
+echo "building"
+jekyll build --quiet --full-rebuild
 
+echo "syncing"
 rsync -ar --rsh='ssh -p22334' --delete-after --delete-excluded  _site/  ed@inkdroid.org:/var/www/inkdroid.org
 
