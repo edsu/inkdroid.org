5847c2c91e8ec3240cda34b6f17b1f4940ff7df2
Ed Summers <ehs@pobox.com>
2016-11-03 14:02:49 -0400

    leave things that happen to be there

--- sync.sh
+++ sync.sh
@@ -5,4 +5,4 @@ echo "building"
 JEKYLL_ENV=production jekyll build --quiet
 
 echo "syncing"
-rsync -ar --rsh='ssh -p22334' --delete-after --delete-excluded  _site/  ed@inkdroid.org:/var/www/inkdroid.org
+rsync -ar --rsh='ssh -p22334' --delete-after _site/  ed@inkdroid.org:/var/www/inkdroid.org
